ccb6e8858061532ea59257c659bd0b1f5f1b8fd8
Sira <sgonzmad@myuax.com>
2024-02-22 20:59:47 +0100

    codigo ejercicio2

---
+++ src/ejercicio2/codigo/TableroOperaciones.java
@@ -0,0 +1,8 @@
+package ejercicio2.codigo;
+
+public interface TableroOperaciones {
+    char[][] crearTablero();
+    void inicializarTablero(char[][] tablero);
+
+    void mostrarTablero(char[][] tablero);
+}
