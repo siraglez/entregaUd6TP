aef3b07fdd1674287c83d7401c1182eaef92a917
Sira <sgonzmad@myuax.com>
2024-02-25 19:08:50 +0100

    otros cambios

--- src/ejercicio4/codigo/Movimiento.java
+++
@@ -1,8 +0,0 @@
-package ejercicio4.codigo;
-
-//Interfaz para definir el comportamiento de los movimientos
-public interface Movimiento {
-    void realizarMovimiento(Tablero tablero, Posicion posicion);
-
-    boolean esMovimientoValido(Tablero tablero, Posicion posicion);
-}
